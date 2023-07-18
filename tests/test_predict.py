6e6db367ef470c51f4adce524cedfc8afbdc5a8e
Aurélien Partiot <aurelien.partiot@orange.com>
2023-07-18 12:00:33 +0200

    update: Add predict from text and html

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,7 +1,11 @@
 from api.nlp.predict import get_topics_from_html, get_topics_from_text
 
 
-def test_get_topics_from_html():
-    html = "<p>consider the <code>pd.Series</code> <code>s</code></p> <pre><code>import pandas as pd import</code></pre><p>"
-    res = get_topics_from_html(html)
-    assert res == ['file', 'data', 'image', 'application', 'app']
+def test_get_topics_from_has_same_prediction():
+    html = "<p>consider the <code>pd.Series</code>  data <code>s</code></p> <pre><code>import pandas as pd import</code></pre><p>"
+    res_html = get_topics_from_html(html)
+    text = "consider the data"
+    res_text = get_topics_from_html(text)
+
+    assert res_html == res_text
+    assert len(res_html) == 5
